6d53f80a563a825d0b77a21bab921cd38ef8be8e
tannerj <codingfrogs@yahoo.com>
2023-06-20 22:23:36 +0100

    feat: Add other message types to example 02

--- sections/01-learning/02-message.cmake
+++ sections/01-learning/02-message.cmake
@@ -1,6 +1,7 @@
-# Execute with: cmake -P CMakeLists.txt
+#!/bin/cmake -P
+# See: https://cmake.org/cmake/help/latest/command/message.html
 
-message("Hello")
+message("0. Hello")
 message([[
 1. A multi line
 2. message
@@ -11,3 +12,10 @@ message([=[
 5. But we have to end it,
 6. the way we started it, i.e. with a bracket+equals+bracket.
 ]=])
+
+# Messages can have a mode, e.g. STATUS, WARNING, NOTICE, FATAL_ERROR
+
+message( STATUS "7. Just a status line" )
+message( WARNING "8. A warning line" )
+message( NOTICE "9. A notice line" )
+message( FATAL_ERROR "10. This will do a stop!" )
