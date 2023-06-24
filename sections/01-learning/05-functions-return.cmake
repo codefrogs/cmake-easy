5e8610f247ca82ab41b2578415d8cbdd08b983f0
tannerj <codingfrogs@yahoo.com>
2023-06-24 21:49:02 +0100

    feat: Add extra function examples

--- sections/01-learning/05-functions-return.cmake
+++ sections/01-learning/05-functions-return.cmake
@@ -2,10 +2,11 @@
 # Cmake-easy by Codefrogs
 #
 message( "Cmake-easy by Codefrogs")
-message( "" )
+message( "Passing parameters, and returning values." )
 
 # ----------------------------------
-# Setting a local variable 
+# Setting a local variable
+# No parameter is passed, or returned.
 function( get_user_name)
   set(var "Nathan" ) # var here is local
 endfunction()
@@ -15,25 +16,47 @@ get_user_name()
 message( "1: User name: ${var}")
 
 # ----------------------------------
-# Setting a parent variable
+# Parsing just a single parameter 
+function( show_name name )
+  message( "2: Name: ${name}" )
+endfunction()
+
+set(user "Karl")
+show_name( ${user} )
+
+# ----------------------------------
+# Parsing multiple parameters
+function( add_record name dob postcode )
+  message( "3: Add record: Name: ${name}, DOB: ${dob}, Postcode: ${postcode}" )
+endfunction()
+
+set(surname "Woods")
+set(dob "31.1.2001")
+set(postcode "WR21 7ER")
+add_record( "${surname}" "${dob}" "${postcode}" )
+
+# ----------------------------------
+# Returning a value from a function
+# The function sets the parent scope variable 'animal'.
 function( get_animal )
-  set(animal "Gorilla" PARENT_SCOPE ) # var here is local
+  set(animal "Gorilla" PARENT_SCOPE )
 endfunction()
 
 set(animal "Unknown")
 get_animal()
-message( "2: User name: ${animal}")
+message( "4: User name: ${animal}")
 
 # ----------------------------------
-# Returning a variable in a global variable
-function( get_planet name )
-  set(${name} "Jupiter" PARENT_SCOPE ) # var here is local
+# Passing a parameters, and getting a return value.
+function( get_planet index name )
+  set(planets "Mercury;Venus;Earth;Mars;Jupiter;Saturn;Uranus;Neptune;Pluto;PlanetX")
+  list(GET planets ${index} planet)
+  set(${name} ${planet} PARENT_SCOPE ) # var here is local
 endfunction()
 
-set(planet "Unknown")
-get_planet("planet")
-message( "3: Planet name: ${planet}")
-
+set(my_planet "Unknown")
+get_planet(2 "my_planet") # Give the return variable name.
+message( "5: Home world: ${my_planet}")
 
 # ----------------------------------
 # Still returning an integer as a string
@@ -42,14 +65,16 @@ function( get_flight_speed speed_var)
 endfunction()
 
 get_flight_speed(speed)
-message( "2: Flight BA734 - Speed: ${speed}")
+message( "6: Flight BA734 - Speed: ${speed}")
 
+# ----------------------------------
+# Returning two values
 function( get_mp3_filenames file file2)
 	set(${file} "sine.mp3" PARENT_SCOPE)
 	set(${file2} "saw.mp3" PARENT_SCOPE)
 endfunction()
 
 get_mp3_filenames(f1 f2)
-message( "6: Filename: ${f1}" )   
-message( "7: Filename: ${f2}")
+message( "7: Filename: ${f1}" )
+message( "8: Filename: ${f2}" )
 
