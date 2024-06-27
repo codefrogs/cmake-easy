9ecfeb5f4c98760282890f1f1a720734957950bf
tannerj <codingfrogs@yahoo.com>
2024-06-27 19:20:14 +0100

    fix: add space to line in message-quotes example

--- sections/01-learning/scripts/03-message-quotes.cmake
+++ sections/01-learning/scripts/03-message-quotes.cmake
@@ -3,7 +3,7 @@
 #
 # Using message() with double quotes
 
-message("1.This is a simple message")
+message("1. This is a simple message")
 message("2. But we can add other characters like tab \t here \t and there.")
 message("3. Or we can add new lines too with a \n4. Not the last line!") 
 message("4. We can also escape characters with a backslash, e.g. \\n")
