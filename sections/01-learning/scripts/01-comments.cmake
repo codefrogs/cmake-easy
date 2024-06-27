f679c99dfd45f598d640724a70d45daa4dfd9e73
tannerj <codingfrogs@yahoo.com>
2024-06-27 18:59:25 +0100

    feat: Expand comment examples

--- sections/01-learning/scripts/01-comments.cmake
+++ sections/01-learning/scripts/01-comments.cmake
@@ -6,5 +6,19 @@ message("Hello world")
   multiline comment
 #]=]
 
+#[===[
+  Another comment, but with 3 equal signs.
+#]===]
 
+# You can put a comment at the end of a command too!
 
+message("Hi again") # End of command comment
+
+# Even multi line comments can be put after a command:
+
+message("Last message (almost)" ) #[==[
+ This is my multi-line comment.
+ We'll keep it short!
+#]==]
+
+message("That's all for now folks!")
