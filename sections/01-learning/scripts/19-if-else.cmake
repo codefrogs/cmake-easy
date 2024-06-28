74a93ff779d15274e626829c836f2b6c0010d6d2
tannerj <codingfrogs@yahoo.com>
2024-06-28 21:17:04 +0100

    feat: Add extra if example

--- sections/01-learning/scripts/19-if-else.cmake
+++ sections/01-learning/scripts/19-if-else.cmake
@@ -6,7 +6,7 @@ message("cmake-easy by Codefrogs")
 # Simple if-endif
 set(colour "white")
 if( ${colour} STREQUAL "white")
-  message("1: STREQUAL test: colour = ${colour}")
+  message("1: if (\${<var>} STREQUAL <str> ) test.")
 endif()
 
 # --- special values ---
@@ -37,19 +37,19 @@ message("------------------------")
 if (0)
   message(${wont_get_here})
 else()
-	message("7: Condition: 0     => false")
+  message("7: Condition: 0      => false")
 endif()
 
 if (OFF)
   message(${wont_get_here})
 else()
-  message("8: Condition: OFF   => false")
+  message("8: Condition: OFF    => false")
 endif()
 
 if (NO)
   message(${wont_get_here})
 else()
-  message("9: Condition: NO    => false")
+  message("9: Condition: NO     => false")
 endif()
 
 if (FALSE)
@@ -75,11 +75,11 @@ if (${var1} STREQUAL "high" AND ${var2} STREQUAL "low" )
   message("12: if <condition> AND <condition> example.")
 endif()
 
-# if-else <undefined variable> example
+# if-else ${<undefined variable>} example
 if ( ${undefined_variable}} ) 
   message(${wont_get_here})
 else()
-  message("13: if (<undefined var>): => equates to false!")
+  message("13: if (\$<undefined var>): => empty string equates to false")
 endif()
 
 # if-else <defined variable> example
@@ -87,12 +87,24 @@ set(my_string_var "text") # defined
 if ( ${my_string_var} ) 
   message(${wont_get_here})
 else()
-  message("14: if (<var>): => still equates to false!!!") # Surprise!
+  message("14: if (\${<var>}): => still equates to false!!!") # Surprised!?
+endif()
+
+if ( my_string_var ) # variable name is defined
+  message("15: if (<var>): => equates to true.")
+else()
+  message(${wont_get_here}) # Surprise!?
+endif()
+
+if ( some_undefined_variable_name ) 
+  message(${wont_get_here})
+else()
+  message("16: if <unknown> => equates to false.") # Surprised!?
 endif()
 
 # DEFINED condition
 if ( DEFINED my_string_var ) 
-  message("15: DEFINED <var> example") # 'my_string_var' is defined
+  message("17: if (DEFINED <known var>) =>  equates to true")
 else()
   message(${wont_get_here})
 endif()
@@ -102,7 +114,7 @@ set(my_defined_var 42)
 if ( ${undefined_variable} )
   message( ${wont_get_here})
 elseif ( DEFINED my_defined_var )
-  message( "16: if-elseif DEFINED <var> example")
+  message( "18: if-elseif DEFINED <var> example")
 endif()
 
 # if-elseif-else example
@@ -111,5 +123,5 @@ if ( ${undefined_variable} )
 elseif ( ${undefined_variable} )
   message( ${wont_get_here} )
 else()
-  message( "17: if-elseif-else example")
+  message( "19: if-elseif-else example")
 endif()
