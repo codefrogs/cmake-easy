c49a9d99c753ce6c9744a41b758256d45ce0e353
tannerj <codingfrogs@yahoo.com>
2024-06-29 18:02:51 +0100

    refactor: Add extra comments and simplify integer test examples

--- sections/01-learning/scripts/09-test-integers.cmake
+++ sections/01-learning/scripts/09-test-integers.cmake
@@ -5,29 +5,40 @@ How to test the value of integers.
 Run: cmake -P CMakeLists.txt
 #]=]
 
-# Here we show how to compare integers (not strings!)
-
-set(DEPTH 25)
-if ( ${DEPTH} EQUAL 25 )
-	message("1. Depth is 25 metres.")
-endif()
+# Numbers comparisons:
+# -GREATER (greater than)
+# -LESS (less than)
+# -EQUAL (equal)
+# -NOT <comparision>
+# -LESS_EQUAL (less or equal to)
+# -GREATER_EQUAL (greater or equal to)
 
 set(level 12)
 if ( ${level} GREATER 8) 
-	message( "2. Reporting level > 8!")
+	message( "1. if <var> GREATER example")
 endif()
 
 set(space 0)
 if ( ${space} LESS 10 )
-	message("3. Space is below 10%!")
+  message("2. if <var> LESS example")
 endif()
 
-set (cpu_temp 85) 
-if ( ${cpu_temp} GREATER_EQUAL 85 )
-	message ("4. CPU is overheating.")
+set(DEPTH 25)
+if ( ${DEPTH} EQUAL 25 )
+  message("3. if <var> EQUAL example")
+endif()
+
+set(DEPTH 25)
+if ( NOT ${DEPTH} EQUAL 0 )
+  message("4. if NOT <var> EQUAL example")
 endif()
 
 set (cpu_usage 5)
 if ( ${cpu_usage} LESS_EQUAL 5 )
-	message ("5. CPU idle.")
+  message ("5. if <var> LESS_EQUAL example")
 endif ()
+
+set (cpu_temp 105) 
+if ( ${cpu_temp} GREATER_EQUAL 85 )
+  message ("4. if <var> GREATER_EQUAL example")
+endif()
