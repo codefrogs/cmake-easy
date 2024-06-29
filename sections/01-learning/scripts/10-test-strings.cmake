82aec266168e7b5b7cc4b51c9d6478f6e215ce56
tannerj <codingfrogs@yahoo.com>
2024-06-29 18:16:00 +0100

    refactor: Add extra comments, and simplify output

--- sections/01-learning/scripts/10-test-strings.cmake
+++ sections/01-learning/scripts/10-test-strings.cmake
@@ -6,6 +6,13 @@
 # See: https://cmake.org/cmake/help/latest/command/if.html#comparisons
 # See: https://cmake.org/cmake/help/latest/command/if.html#version-comparisons
 
+# String comparisons:
+# -STRGREATER (greater than)
+# -STRLESS (less than)
+# -STREQUAL (equal)
+# -STRLESS_EQUAL (less or equal to)
+# -STRGREATER_EQUAL (greater or equal to)
+
 message("cmake-easy by Codefrogs")
 message("String comparisions")
 
@@ -14,30 +21,33 @@ set(name2 "Pascal")
 if ( ${name1} STREQUAL ${name2} ) 
   message( "X.Same name!" )
 else()
-  message( "1. Different" )
+  message( "1. if <var> STREQUAL <var> example" )
 endif()
 
 # Ordering of strings
-# Notice the backward-slash for the greater sign.
 set( apple "Apple" )
 set( pinea "Pinapple" )
 
 if ( ${apple} STRGREATER ${pinea} )
   message( "X. Apple comes after Pinapple?!" )
 else()
-  message( "2. Pinapple comes after Apple." )
+  message( "2. if <var> STRGREATER <var> example" )
 endif()
 
 if ( ${apple} STRLESS ${pinea} )
-  message( "3. Apple is less than Pinapple." )
+  message( "3. if <var> STRLESS <var> example" )
 endif()
 
 set(other "Apple")
 if ( ${apple} STREQUAL ${other} )
-  message( "4. Apple is the same as Apple." )
+  message( "4. if <var> STREQUAL <var> example." )
+endif()
+
+if ( ${apple} STRLESS_EQUAL "Apple" )
+  message( "5. if <var> STRLESS_EQUAL <var> example." )
 endif()
 
 set(pear "Pear")
 if ( NOT ${apple} STREQUAL ${pear} )
-  message( "5. Apple is not a Pear." )
+  message( "6. if NOT <var> STREQUAL <var> example" )
 endif()
