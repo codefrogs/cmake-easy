17a4bfe48b628a17faecc3c93dd13a83a3c2ed4a
tannerj <codingfrogs@yahoo.com>
2024-07-02 12:47:11 +0100

    feat: Add execute_process example

---
+++ sections/01-learning/scripts/18-command-execute.cmake
@@ -0,0 +1,23 @@
+# cmake-easy by Codefrogs
+
+# Using 'execute-process' to get the user and list of processes
+# This will only work on Linux systems.
+# see: https://cmake.org/cmake/help/latest/command/execute_process.html
+
+# Setting a variable to hold the command
+set(who_am_i_cmd "whoami")
+
+message("Running 'whoami' command:")
+execute_process(COMMAND ${who_am_i_cmd} OUTPUT_VARIABLE user)
+
+message("User is: ${user}")
+
+#Get all processes
+set(process_cmd "ps")
+set(process_args "-h")
+
+message("Running 'ps' command")
+execute_process(COMMAND ${process_cmd} ${process_args} OUTPUT_VARIABLE processes)
+
+message("---Processes---")
+message("${processes}")
