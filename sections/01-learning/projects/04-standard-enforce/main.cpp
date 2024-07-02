15dbf41b8078293411d6cda81130bf13fa80b5f7
tannerj <codingfrogs@yahoo.com>
2024-07-02 19:22:49 +0100

    feat: Add 'enforce compiler standard' example

---
+++ sections/01-learning/projects/04-standard-enforce/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using std::endl;
+using std::cout;
+
+// Example uses the C++20 space-ship operator. 
+//
+int main()
+{
+  int level_previous{3};
+  int level_current{2};
+
+  auto level_change = level_current <=> level_previous;
+  cout << "C+20 three-way operator example (again!)" << endl;
+  cout << "----------------------------------------\n\n";
+
+  if ( level_change < 0 )     cout << "Level reducing" << endl;
+  else if (level_change > 0 ) cout << "Level increasing" << endl;
+  else cout << "Level stable." << endl;
+
+  return 0;
+}
+
+
